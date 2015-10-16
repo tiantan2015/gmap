aab3bf3beaba46412f625e5d3c9add248fa34de9
tiantan <tiantan@SC-LAB361.marvell.com>
2015-10-16 11:05:51 -0700

    tool to convert gmap xml file to text, and including latitude for each item

---
+++ gmap2txt.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+use strict;
+# use module
+use XML::Simple;
+use Data::Dumper;
+
+# usage:
+#      perl gmap2txt.pl doc.kml > doc.txt
+# doc.kml can be extracted from kmz file
+&main();
+
+#sub Unicode2Ascii
+#{
+#   my $string = shift;
+#   $string =~ s/([^[:ascii:]]+)/unidecode($1)/ge;
+#   return $string;
+#}
+
+sub NumberIndexToAlphabeta
+{
+   my $index = shift;
+   $index = $index % 52;
+   if( $index < 26)
+   {
+      return chr(65+$index);
+   }
+   else
+   {
+      return chr(97+$index-26);
+   }
+}
+
+sub main
+{
+   my $xml = new XML::Simple;
+   binmode STDOUT, ":utf8";
+
+   # read XML file
+   #my $data = $xml->XMLin("doc.kml", ForceArray => 1);
+   my $data = $xml->XMLin( $ARGV[0], ForceArray => 1);
+
+   foreach my $map (@{$data->{'Document'}})
+   {
+      #print Dumper($map);
+      print "Map ".$map->{'name'}[0]."\n";
+      foreach my $folder (@{ $map->{'Folder'}})
+      {
+         #print Dumper($folder);
+         print "\nLayer ".$folder->{'name'}[0]."\n";
+
+         my $style = $folder->{'Placemark'}[0]{'styleUrl'}[0];
+         
+         my $index = 0;
+         foreach my $place (@{$folder->{'Placemark'}})
+         {
+            #print Dumper($place);         
+            #my $style = $place->{'styleUrl'}[0];
+            #if( $style =~ /(\d+)/  )
+            #{
+            #   $index = $1;
+            #}
+            my $description = '';
+            if( exists $place->{'description'})
+            {
+               $description = $place->{'description'}[0];
+            }
+
+            print NumberIndexToAlphabeta($index);
+            print " ";
+
+            print $place->{'name'}[0]." ".$place->{'Point'}[0]{'coordinates'}[0]."\n";
+            if( $description =~ /\S+/)
+            {
+               $description =~ s/<br>/\n/gs;
+               $description =~ s/\n/\t/gs;
+               print "\t".$description."\n";
+            }
+            
+            $index++;
+         }
+      }
+   }
+}
